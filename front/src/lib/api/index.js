e37c43a8cc0c79d1470087ac22a61b171fdb571a
Samujal Phukan <samujalphukan@yahoo.com>
2026-03-11 21:13:02 +0530

    asa

--- front/src/lib/api/index.js
+++
@@ -1,6 +0,0 @@
-// src/lib/api/index.js
-export { default as apiClient } from './client';
-export { authApi } from './auth';
-export { servicesApi } from './services';
-export { bookingsApi } from './bookings';
-export { reviewsApi } from './reviews';
--- front/src/store/index.js
+++
@@ -1,4 +0,0 @@
-// src/store/index.js
-export { useAuthStore } from './authStore';
-export { useBookingStore } from './bookingStore';
-export { useUIStore } from './uiStore';
