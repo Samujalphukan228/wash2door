69d6c0d86a09b86ef6f8554a4674c0558c89acfa
Samujal Phukan <samujalphukan@yahoo.com>
2026-03-17 01:17:35 +0530

    ass

--- front/src/context/TransitionContext.jsx
+++ front/src/context/TransitionContext.jsx
@@ -35,7 +35,7 @@ export function TransitionProvider({ children }) {
         readyRef.current = true
         setStage("out")
       }
-    }, 250)
+    }, 300)
   }, [])
 
   const signalReady = useCallback(() => {
